4f7c41b4722d15aa8609c7c82c26d92fa2465449
Tomás Afonso <168651587+thomraider12@users.noreply.github.com>
2025-12-21 19:41:02 +0000

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -3,8 +3,7 @@
 echo $(dirname $0)
 
 python3 -m pip install yt-dlp
-cd $(dirname $0)/scripts/
 
-python youtube_m3ugrabber.py -i youtube_channel_info.txt
+python scripts/youtube_m3ugrabber.py -i ../youtube_channel_info.txt
 
 echo m3u grabbed
